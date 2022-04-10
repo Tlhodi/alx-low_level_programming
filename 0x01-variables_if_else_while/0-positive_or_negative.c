78dc3a9f0e8761f845dd3dd93fa19594d9613372
Dominic Raborokgwe <rabotlhodi@yahoo.com>
2022-04-10 13:27:20 -0700

    if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand()- RAND_MAX / 2;
-	if(n > 0) {printf("%d is positive \n", n)}
-	else if(n < 0) {printf("%d is negative \n", n)}
-	else {printf("%d is zero \n", n)};
+	if(n > 0) {puts("%d is positive \n", n)}
+	else if(n < 0) {puts("%d is negative \n", n)}
+	else {puts("%d is zero \n", n)};
 	return (0);
 }
