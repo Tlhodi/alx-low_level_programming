4fd960eb6ee075c1b812ebb36da318fe953af44d
Dominic Raborokgwe <rabotlhodi@yahoo.com>
2022-04-12 19:18:49 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,22 @@
-/*
- * File: 0-putchar.c
- * Auth: Dominic R
- *
- */
-#include <main.h>
-#include "_putchar.h"
+#include "main.h"
 
 /**
- * main - Prints "_putchar" followed by a new line.
+ * main - prints _putchar
  *
- * Return: return 0.
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
 
 	return (0);
