177c4e96a82331a9637ecc0f08eaf477d455a72d
Dominic Raborokgwe <rabotlhodi@yahoo.com>
2022-04-13 17:43:29 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "print_alphabet_x10.c"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
